3138ccde62755da220c91ddbd683c30d94d4e180
vatsal-kandoi <vatsalkandoi1998@gmail.com>
2024-02-28 23:54:23 +0530

    Added support for spanish

--- src/hooks/languageprovider.tsx
+++ src/hooks/languageprovider.tsx
@@ -2,20 +2,32 @@ import * as React from "react";
 import { Languages } from "../interfaces/enums";
 import createLanguageStore from "../i18n";
 import LanguageProviderContext from "../contexts/languageprovider";
+import { I18n } from "i18n-js";
 
 export function useLanguageStore() {
     const context = React.useContext(LanguageProviderContext);
-    return context.i18n;
+    return context.languageStore.store;
+}
+
+export function useLanguageCode() {
+    const context = React.useContext(LanguageProviderContext);
+    return context.languageStore.code;
 }
 
 export function useLanguageProvider() {
-    const i18n = createLanguageStore();
+    const [languageStore, setLanguageStore] = React.useState<{ code: Languages, store: I18n }>({
+        code: Languages.EN,
+        store: createLanguageStore("en")
+    })
 
     const languageContextValue = {
         toggleLanguage: (language: Languages) => {
-            i18n.locale = (language as string);
+            setLanguageStore({
+                code: language,
+                store: createLanguageStore(language)
+            });
         },
-        i18n: i18n
+        languageStore: languageStore
     }
     
     return languageContextValue;
--- src/i18n.ts
+++ src/i18n.ts
@@ -1,13 +1,15 @@
 import { I18n } from "i18n-js";
 import enTranslations from "./translations/en";
+import espTranslations from "./translations/esp";
 
-export default function createLanguageStore() {
+export default function createLanguageStore(lang: string) {
     const i18n = new I18n({
-        en : enTranslations
+        en : enTranslations,
+        esp: espTranslations
     });
     
-    i18n.defaultLocale = "en";
-    i18n.locale = "en";
+    i18n.defaultLocale = lang;
+    i18n.locale = lang;
     i18n.enableFallback = true;
     
     return i18n;
